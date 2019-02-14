d3ce10537c553c560eef57ee5776eaa3f7cb2048
Daniel Bartholomae <daniel@bartholomae.name>
2019-02-14 12:39:09 +0100

    feat: check if token is valid

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,18 @@
+import { Middleware, MiddlewareFunction } from 'middy';
+import { IAuthorizedEvent } from './IAuthorizedEvent';
+export interface IAuthOptions {
+    key: string;
+}
 /** A documented example module */
-export default class Module {
+export declare class JWTAuthMiddleware {
+    private options;
     /** The logger used in the module */
     private readonly logger;
+    static create: Middleware<IAuthOptions>;
     /** Creates a new Module */
-    constructor();
+    constructor(options: IAuthOptions);
     /** Starts the module */
-    start(): void;
+    before: MiddlewareFunction<IAuthorizedEvent>;
 }
+declare const _default: Middleware<IAuthOptions, any, any>;
+export default _default;
